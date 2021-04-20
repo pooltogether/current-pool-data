b10a80f5c79a54e2e7ce1281a15dd879670c67eb
Dylan DesRosier <dylan@pooltogether.com>
2021-04-20 16:30:37 -0400

    add list

---
+++ prizePoolContracts.js
@@ -0,0 +1,125 @@
+// List of prize pools for the flagship app
+
+module.exports = {
+  1: {
+    governance: [
+      {
+        prizePool: {
+          address: '0xebfb47a7ad0fd6e57323c8a42b2e5a6a4f68fc1a'
+        },
+        symbol: 'PT-cDAI',
+        subgraphVersion: '3.1.0'
+      },
+      {
+        prizePool: {
+          address: '0x0650d780292142835f6ac58dd8e2a336e87b4393'
+        },
+        symbol: 'PT-cUNI',
+        subgraphVersion: '3.1.0'
+      },
+      {
+        prizePool: {
+          address: '0xde9ec95d7708b8319ccca4b8bc92c0a3b70bf416'
+        },
+        symbol: 'PT-cUSDC',
+        subgraphVersion: '3.1.0'
+      },
+      {
+        prizePool: {
+          address: '0x396b4489da692788e327e2e4b2b0459a5ef26791'
+        },
+        symbol: 'PT-stPOOL',
+        subgraphVersion: '3.3.2'
+      },
+      {
+        prizePool: {
+          address: '0xbc82221e131c082336cf698f0ca3ebd18afd4ce7'
+        },
+        symbol: 'PT-cCOMP',
+        subgraphVersion: '3.1.0'
+      }
+    ],
+    community: [
+      {
+        prizePool: { address: '0xa88ca010b32a54d446fc38091ddbca55750cbfc3' },
+        symbol: 'WETH-0xa88ca0',
+        subgraphVersion: '3.3.2'
+      },
+      {
+        prizePool: { address: '0xea7eaecbff99ce2412e794437325f3bd225ee78f' },
+        symbol: 'BOND-0xea7eae',
+        subgraphVersion: '3.1.0'
+      },
+      {
+        prizePool: { address: '0xdf19f2f606dcc5849199594e77058898a7caa73d' },
+        symbol: 'ZRX-0xdf19f2',
+        subgraphVersion: '3.1.0'
+      },
+      {
+        prizePool: { address: '0x639d4140a1f7723b7cefef7505d1d7be11a43de0' },
+        symbol: 'UNI-V2-0x639d41',
+        subgraphVersion: '3.1.0'
+      },
+      {
+        prizePool: { address: '0x481f1ba81f7c01400831dff18215961c3530d118' },
+        symbol: 'USDT-0x481f1B',
+        subgraphVersion: '3.3.8'
+      },
+      {
+        prizePool: { address: '0x9f7905c7bd5ec9e870ed50f0e286f2742c19f5b3' },
+        symbol: 'DPI-0x9f7905',
+        subgraphVersion: '3.1.0'
+      },
+      {
+        prizePool: { address: '0xc2a7dfb76e93d12a1bb1fa151b9900158090395d' },
+        symbol: 'PT-stBADGER',
+        subgraphVersion: '3.3.2'
+      }
+    ]
+  },
+  4: {
+    governance: [
+      {
+        prizePool: {
+          address: '0x4706856fa8bb747d50b4ef8547fe51ab5edc4ac2'
+        },
+        symbol: 'PT-cDAI',
+        subgraphVersion: '3.1.0'
+      },
+      {
+        prizePool: {
+          address: '0xab068f220e10eed899b54f1113de7e354c9a8eb7'
+        },
+        symbol: 'PT-cBAT',
+        subgraphVersion: '3.1.0'
+      }
+    ],
+    community: [
+      {
+        prizePool: { address: '0x95bca36b53ab0c54b162672454fe4be869a6f9ca' },
+        symbol: 'USDC-0x95bca3',
+        subgraphVersion: '3.3.2'
+      },
+      {
+        prizePool: { address: '0xc8e1ea1afb7361cd647ad1a54a6c074f1174eb6e' },
+        symbol: 'BAT-0xc8e1ea',
+        subgraphVersion: '3.3.2'
+      },
+      {
+        prizePool: { address: '0x84ed0f89c033fe7dadfc4d5f2a516ebd9dc15644' },
+        symbol: 'DAI-0x84ed0f',
+        subgraphVersion: '3.3.8'
+      }
+    ]
+  },
+  137: {
+    governance: [],
+    community: [
+      {
+        prizePool: { address: '0x60764c6be24ddab70d9ae1dbf7436533cc073c21' },
+        symbol: 'DAI-0x60764c',
+        subgraphVersion: '3.3.0'
+      }
+    ]
+  }
+}
