ac42718af42d6a13832cca52b4be35a0384aae8e
Charles Bergeron <chuck.bergeron@gmail.com>
2021-04-20 14:07:09 -0700

    * export the new data struct

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 var contractAddresses = require('./contractAddresses')
 var contractVersions = require('./contractVersions')
 var prizePoolBuilders = require('./prizePoolBuilders')
+var prizePoolContracts = require('./prizePoolContracts')
 
 module.exports = {
   contractAddresses,
   contractVersions,
-  prizePoolBuilders
+  prizePoolBuilders,
+  prizePoolContracts
 }
