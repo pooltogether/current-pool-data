353b70a12f0e2a7231c6a06355ffa62dc16c4b69
Dylan DesRosier <dylan@pooltogether.com>
2021-05-20 15:36:40 -0400

    fix export

--- subgraphUris.js
+++ subgraphUris.js
@@ -1,4 +1,4 @@
-export const POOLTOGETHER_SUBGRAPH_URIS = {
+const POOLTOGETHER_SUBGRAPH_URIS = {
   1: {
     '3.1.0': 'https://api.thegraph.com/subgraphs/name/pooltogether/pooltogether-v3_1_0',
     '3.3.2': 'https://api.thegraph.com/subgraphs/name/pooltogether/pooltogether-v3_3_2',
@@ -18,13 +18,13 @@ export const POOLTOGETHER_SUBGRAPH_URIS = {
   }
 }
 
-export const LOOTBOX_GRAPH_URIS = {
+const LOOTBOX_GRAPH_URIS = {
   1: 'https://api.thegraph.com/subgraphs/name/pooltogether/lootbox-v1_0_0',
   3: '',
   4: 'https://api.thegraph.com/subgraphs/name/pooltogether/ptv3-lootbox-rinkeby-staging'
 }
 
-export const UNISWAP_GRAPH_URIS = {
+const UNISWAP_GRAPH_URIS = {
   1: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
   4: 'https://api.thegraph.com/subgraphs/name/blockrockettech/uniswap-v2-subgraph-rinkeby',
   137: 'https://api.thegraph.com/subgraphs/name/sameepsi/quickswap'
